f8aaf1295aeadb370b113f4685a6cde633157174
yangyang <yangeon@163.com>
2018-04-23 02:16:03 -0400

    nothing to say...

---
+++ js/generateContent.js
@@ -0,0 +1,37 @@
+$.getJSON("../data/content.json", (data) => {
+  loopAllProjects(data);
+});
+
+var loopAllProjects = (data) => {
+  var items = [];
+  $.each(data.projects, (index, value) => {
+    generateContent(value);
+  });
+  // $("<ul/>", {
+  //   "class": "my-new-list",
+  //   html: items.join("")
+  // }).appendTo("body");
+}
+
+var generateContent = (project) => {
+  let block,
+    cover,
+    title;
+// generate block
+  block = $("<div>", {
+    "class": "block"
+  });
+// generate cover element, then append it to block
+  switch (project.coverFormate) {
+    case "image":
+      cover = $("<img>", {
+        "src": project.coverPath
+      });
+      break;
+    case "video":
+      break;
+  }
+  cover.appendTo(block);
+  block.appendTo("body");
+
+}
