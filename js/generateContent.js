0272e6091b294179eb9871d54cfb1df476ba9983
yangyang <yangeon@163.com>
2019-02-09 13:48:17 -0500

    Feb 9, 2019 neweast

--- js/generateContent.js
+++ js/generateContent.js
@@ -1,35 +1,69 @@
 $.getJSON("data/content.json", (data) => {
-  loopAllProjects(data);
+    loopAllProjects(data);
 });
 
 var loopAllProjects = (data) => {
-  var items = [];
-  $.each(data.projects, (index, value) => {
-    generateContent(value);
-  });
-}
+    var items = [];
+    $.each(data.projects, (index, value) => {
+        generateContent(value);
+    });
+};
 
 var generateContent = (project) => {
-  let block,
-    cover,
-    title;
-// generate block
-  block = $("<div>", {
-    "class": "block vw50"
-  });
-// generate cover element, then append it to block
-  switch (project.coverFormate) {
-    case "image":
-      cover = $("<img>", {
-        "src": project.coverPath
-      });
-      break;
-    case "video":
-      break;
-  }
-  cover.appendTo(block);
-  block.appendTo("body");
+    let block,
+        cover,
+        gifLayer,
+        link,
+        hoverLayer,
+        title;
+    // generate block
+    block = $("<div>", {
+        "class": "block vw50"
+    });
+    // generate hover layer
+    // generate link
+    link = $("<a>").attr("href", project.link);
+    // generate cover element, then append it to block
+    // switch (project.coverFormate) {
+    //     case "image":
+    //
+    //         break;
+    //     case "video":
+    //         break;
+    // }
 
+    cover = $("<img>", {
+        "src": project.cover.image,
+        "class": "cover"
+    });
 
+    gifLayer = $("<img>", {
+        "src": project.cover.animation,
+        "class": "gifLayer block vw50"
+    });
 
-}
+    hoverLayer = $("<div>", {
+        "class": "hoverLayer block vw50"
+        // "class": "block vw50"
+    });
+
+    title = $("<h2>", {
+        "class": "title vw50",
+    });
+    title.append(project.title)
+
+    //put link in to cover
+    // link.appendTo(block);
+    cover.appendTo(block);
+    gifLayer.appendTo(block);
+    hoverLayer.appendTo(block);
+    title.appendTo(block);
+    block.appendTo("body");
+
+    block.click(
+        function() {
+            window.open(
+                project.link, '_blank');
+        }
+    );
+};
--- js/layout.js
+++ js/layout.js
@@ -1,22 +1,24 @@
 let minWidth = 400;
 
 $(window).resize(() => {
-  resize();
+    resize();
 });
 
 var resize = () => {
-  let className = $(".block").attr('class');
+    let className = $(".block").attr('class');
 
-  if ($(window).width() >= minWidth * 2 && className.indexOf("vw50") === -1) {
-    console.log(`> ${minWidth * 2}`);
-    $(".block").removeClass("vw100").addClass("vw50");
-  } else if ($(window).width() < minWidth * 2 && className.indexOf("vw100") === -1) {
-    console.log(`< ${minWidth * 2}`);
-    $(".block").removeClass("vw50").addClass("vw100");
-  }
+    if ($(window).width() >= minWidth * 2 && className.indexOf("vw50") === -1) {
+        console.log(`> ${minWidth * 2}`);
+        $(".block").removeClass("vw100").addClass("vw50");
+        $(".title").removeClass("vw100").addClass("vw50");
+    } else if ($(window).width() < minWidth * 2 && className.indexOf("vw100") === -1) {
+        console.log(`< ${minWidth * 2}`);
+        $(".block").removeClass("vw50").addClass("vw100");
+        $(".title").removeClass("vw50").addClass("vw100");
+    }
 
-}
+};
 
-setTimeout(()=> {
-  resize();
-}, 300);
+setTimeout(() => {
+    resize();
+}, 50);
