acafbb1cfb7fb5bcd52418722f8dcc5a6ffd1d60
yangyang <yangeon@163.com>
2020-01-27 01:57:50 -0500

    auto play video on phone

--- js/vueApp.js
+++ js/vueApp.js
@@ -1,23 +1,51 @@
 var vueApp = {
     el: "#app",
     data: {},
-    methods:{
-        openLink: function (link) {   
-            window.open(link, "_blank");    
+    methods: {
+        openLink: function (link) {
+            window.open(link, "_blank");
         }
-      }
+    }
 }
 
 function isMobile() {
-    try{ document.createEvent("TouchEvent"); return true; }
-    catch(e){ return false; }
-  }
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
 
 vueApp.data.isMobile = isMobile();
 
+var observer = new IntersectionObserver((entries) => {
+    console.log(entries);
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            $(entry.target).find(".darkOverlay").fadeTo("slow", 0);
+            $(entry.target).find("img").fadeTo("slow", 0);
+        } else {
+            $(entry.target).find(".darkOverlay").fadeTo("slow", 0.6);
+            $(entry.target).find("img").fadeTo("slow", 1);
+        }
+
+    });
+}, {
+    threshold: 0.9
+});
 
 $.getJSON("data/content.json", (res) => {
     console.log(res);
     vueApp.data.projects = res.projects;
     var app = new Vue(vueApp);
-});
+}).then(startObserve);
+
+
+function startObserve() {
+    if (vueApp.data.isMobile) {
+        $(".coverContainer").each((index, element) => {
+            observer.observe(element);
+        })
+    }
+}
