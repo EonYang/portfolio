96cbac1edfeb15defb97325b46fdc6b1aba64305
yangyang <yangeon@163.com>
2020-01-27 02:39:42 -0500

    video will play one by one

--- js/vueApp.js
+++ js/vueApp.js
@@ -19,22 +19,36 @@ function isMobile() {
 
 vueApp.data.isMobile = isMobile();
 
+var elementsInView = [];
 var observer = new IntersectionObserver((entries) => {
-    console.log(entries);
+    // console.log(entries);
+    let currentFirstEl = elementsInView[0];
+    console.log(elementsInView.length);
+    console.log(currentFirstEl);
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            setTimeout(()=>{
-                $(entry.target).find(".darkOverlay").fadeTo("slow", 0);
-            $(entry.target).find("img").fadeTo("slow", 0);
-
-            },1200)
-            
+            elementsInView.push(entry.target);
         } else {
-            $(entry.target).find(".darkOverlay").fadeTo("slow", 0.6);
-            $(entry.target).find("img").fadeTo("slow", 1);
+            let spliceInd = $.inArray(entry.target, elementsInView);
+            console.log("spliceInd");
+            console.log(spliceInd);
+            if (spliceInd >= 0) {
+                elementsInView.splice(spliceInd, 1);
+            }
         }
-
     });
+    if (elementsInView[0] != undefined && elementsInView[0] != currentFirstEl) {
+        try {
+            $(currentFirstEl).find(".darkOverlay").fadeTo("slow", 0.6);
+            $(currentFirstEl).find("img").fadeTo("slow", 1);
+        } catch (error) {
+            console.log(error)
+        }
+        $(elementsInView[0]).find(".darkOverlay").fadeTo("slow", 0.2);
+        $(elementsInView[0]).find("img").fadeTo("slow", 0);
+        console.log(elementsInView.length)
+    }
+
 }, {
     threshold: 0.9
 });
